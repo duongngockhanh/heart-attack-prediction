afb10a5c092803048596ed7383f9162a34eadaed
duongngockhanh <khanhduongsc@gmail.com>
2023-10-14 10:20:19 +0700

    udpate new b9_stacking

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# heart-attack-prediction
+# Heart Attack Prediction
 Synthesize Decision Tree algorithms and their variations to predict the likelihood of developing heart disease.
--- b9_stacking.py
+++ b9_stacking.py
@@ -13,52 +13,37 @@ df = get_dataframe()
 # Tách dữ liệu thành tập huấn luyện và tập kiểm tra
 X = df.iloc[:, :-1].values
 y = df.iloc[:, -1].values
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Khởi tạo các mô hình cơ bản
-dtc = DecisionTreeClassifier(random_state=42)
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 42)
+
+dtc =  DecisionTreeClassifier(random_state=42)
 rfc = RandomForestClassifier(random_state=42)
-knn = KNeighborsClassifier()
-xgb = XGBClassifier(objective="binary:logistic", random_state=42, n_estimators=100)
+knn =  KNeighborsClassifier()
+xgb = XGBClassifier(XGBClassifier)
 gc = GradientBoostingClassifier(random_state=42)
-svc = SVC(kernel='rbf', random_state=42)
+svc = SVC(kernel = 'rbf', random_state=42)
 ad = AdaBoostClassifier(random_state=42)
 
-# Sử dụng dự đoán từ các mô hình cơ bản để tạo thành tập dữ liệu mới cho mô hình tổng hợp
-base_learners = [dtc, rfc, knn, xgb, gc, svc, ad]
-X_train_meta = np.zeros((len(y_train), len(base_learners)))
+clf = [('dtc',dtc),('rfc',rfc),('knn',knn), ('gc',gc), ('ad',ad), ('svc', svc)] #list of (str, estimator)
+from sklearn.ensemble import StackingClassifier
+xg = XGBClassifier()
+classifier = StackingClassifier( estimators = clf,final_estimator = xg)
+classifier.fit(X_train,y_train)
 
-for i, model in enumerate(base_learners):
-    model.fit(X_train, y_train)
-    X_train_meta[:, i] = model.predict(X_train)
+classifier.fit(X_train, y_train)
 
-# Huấn luyện mô hình tổng hợp trên tập dữ liệu mới
-meta_learner = RandomForestClassifier(random_state=42)
-meta_learner.fit(X_train_meta, y_train)
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
 
-# Sử dụng dự đoán từ các mô hình cơ bản trên tập kiểm tra để tạo thành tập dữ liệu mới cho mô hình tổng hợp
-X_test_meta = np.zeros((len(y_test), len(base_learners)))
+from sklearn.metrics import confusion_matrix
+cm_test = confusion_matrix(y_pred, y_test)
 
-for i, model in enumerate(base_learners):
-    X_test_meta[:, i] = model.predict(X_test)
+y_pred_train = classifier.predict(X_train)
+cm_train = confusion_matrix(y_pred_train, y_train)
 
-# Dự đoán bằng mô hình tổng hợp
-y_pred = meta_learner.predict(X_test_meta)
-
-# Tính toán độ chính xác và ma trận nhầm lẫn cho cả tập kiểm tra và tập huấn luyện
-cm_test = confusion_matrix(y_test, y_pred)
-accuracy_for_test = np.round((cm_test[0][0] + cm_test[1][1]) / len(y_test), 2)
-
-# Dự đoán trên tập huấn luyện
-X_train_meta = np.zeros((len(y_train), len(base_learners)))
-
-for i, model in enumerate(base_learners):
-    X_train_meta[:, i] = model.predict(X_train)
-
-y_train_pred = meta_learner.predict(X_train_meta)
-
-cm_train = confusion_matrix(y_train, y_train_pred)
-accuracy_for_train = np.round((cm_train[0][0] + cm_train[1][1]) / len(y_train), 2)
-
-print('Accuracy for training set for Stacking = {}'.format(accuracy_for_train))
-print('Accuracy for test set for Stacking = {}'.format(accuracy_for_test))
+print()
+accuracy_for_train = np.round((cm_train[0][0] + cm_train[1][1])/len(y_train),2)
+accuracy_for_test = np.round((cm_test[0][0] + cm_test[1][1])/len(y_test),2)
+print('Accuracy for training set for StackingClassifier = {}'.format(accuracy_for_train))
+print('Accuracy for test set for StackingClassifier = {}'.format(accuracy_for_test))
